fd3eb6a89c48f825eb06d94e2ed87d615e184f6a
BenGale93 <bengale2007@googlemail.com>
2020-09-23 21:39:09 +0100

    Added person_box

--- family_tree/__init__.py
+++ family_tree/__init__.py
@@ -3,3 +3,5 @@ from . import constants
 from .person import Person
 from .couple import Couple
 from .family import Family
+
+from . import cmd_viewer
---
+++ family_tree/cmd_viewer.py
@@ -0,0 +1,34 @@
+import math
+
+from family_tree import Person
+
+
+def person_box(person: Person) -> str:
+    _, width = person.shape()
+
+    top_bottom = "-" * (width + 4) + "\n"
+    blank_line = surround_bars(" " * width, width)
+    lines = [top_bottom, blank_line]
+
+    for key, value in person.info().items():
+        if key == "parents" or key == "spouses":
+            if value:
+                lines.append(surround_bars(f"{key}: ", width))
+                for relative in value.split("\n"):
+                    lines.append(surround_bars(relative, width))
+        else:
+            lines.append(surround_bars(f"{key}: {value}", width))
+
+    lines.append(blank_line)
+    lines.append(top_bottom)
+
+    return "".join(lines)
+
+
+def surround_bars(string: str, width: int) -> str:
+    space_needed = width - len(string)
+
+    left_whitespace = " " * math.floor((space_needed / 2))
+    right_whitespace = " " * math.ceil((space_needed / 2))
+
+    return "| " + left_whitespace + string + right_whitespace + " |\n"
