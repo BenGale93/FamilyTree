f88727f88f4197a4c428d2972e8a45b85ea9239b
BenGale93 <bengale2007@googlemail.com>
2020-10-04 15:28:13 +0100

    Added further tests

--- tests/test_family.py
+++ tests/test_family.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pytest  # type: ignore
 
 import family_tree
+from family_tree import constants
 from . import LOCATION
 
 
@@ -55,3 +56,44 @@ def test_json_validate_fail_keys():
 def test_json_validate_fail_identifier():
     with pytest.raises(KeyError):  # type: ignore
         family_tree.Family.from_json(f"{LOCATION}invalid_id.json")
+
+
+@pytest.fixture
+def relation_test_fam() -> family_tree.Family:
+    return family_tree.Family.from_json("tests\\data\\relationship_test.json")
+
+
+def test_list_ancestors_layout(relation_test_fam: family_tree.Family):
+    assert relation_test_fam.list_ancestors(relation_test_fam["G1A"]) == [
+        {"G2B", "G2A"},
+        {"G3B", "G3A", "G3C", "G3D"},
+        {"G4A"},
+    ]
+
+
+def test_parental(relation_test_fam: family_tree.Family):
+    assert relation_test_fam.relationship("G1A", "G2A") == "Parent"
+
+
+def test_child(relation_test_fam: family_tree.Family):
+    assert relation_test_fam.relationship("G2B", "G1A") == "Child"
+
+
+def test_grandparent(relation_test_fam: family_tree.Family):
+    assert relation_test_fam.relationship("G1A", "G3A") == "Grand-Parent"
+
+
+def test_great_grandparent(relation_test_fam: family_tree.Family):
+    assert relation_test_fam.relationship("G1A", "G4A") == "Great Grand-Parent"
+
+
+def test_siblings(relation_test_fam: family_tree.Family):
+    assert (
+        relation_test_fam.relationship("G1A", "G1B") == constants.RELATION_MATRIX[0][0]
+    )
+
+
+def test_aunt_uncle(relation_test_fam: family_tree.Family):
+    assert (
+        relation_test_fam.relationship("G1A", "G2C") == constants.RELATION_MATRIX[1][0]
+    )
